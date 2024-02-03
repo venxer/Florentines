8f4725abbff84a84377927064efd26aa0d6792a3
Edwin Zhao <99381274+venxer@users.noreply.github.com>
2024-02-03 14:52:31 -0500

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
 import './App.css';
-
+import Miffy from "./miffy.png";
 function App() {
+  const moveBtn = () => {
+    const btn = document.querySelector(".noBtn")
+
+    const viewportWidth = window.innerWidth;
+    const viewportHeight = window.innerHeight;
+
+    // Calculate random positions
+    let randomLeft = Math.random() * (viewportWidth - btn.offsetWidth);
+    let randomTop = Math.random() * (viewportHeight - btn.offsetHeight);
+
+    // Apply the random positions to the btn
+    btn.style.position = "absolute";
+    btn.style.left = `${randomLeft}px`;
+    btn.style.top = `${randomTop}px`;
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <img src={Miffy}
+           className='center'/>
+      <h1>Will you be my Valentines? 💕</h1>
+      <div className='buttons'>
+        <button className="yesBtn"
+                onClick={() => alert("Awwwww Thank You")}>Yes 🥰</button>
+        <button className="noBtn"
+                onMouseOver={() => moveBtn()}
+                onClick={() => moveBtn()}>No 😔</button>
+      </div>
     </div>
   );
 }
